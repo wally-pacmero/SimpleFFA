f5168c86e2897d00a07554e3e5b691f952345ff6
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2022-07-08 14:51:26 -0500

    Update Loader.php

--- src/simpleffa/Loader.php
+++ src/simpleffa/Loader.php
@@ -3,7 +3,19 @@
 namespace simpleffa;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 
 class Loader extends PluginBase {
+ use SingletonTrait;
 
+ protected function onLoad(): void {
+        self::setInstance($this);
+    }
+
+ public function onEnable(): void {
+        $this->registerListener(new FFAListener());
+    }
+ private function registerListener(Listener $listener): void {
+        $this->getServer()->getPluginManager()->registerEvents($listener, $this);
+    }
 }
