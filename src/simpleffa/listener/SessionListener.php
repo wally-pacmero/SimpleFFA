df82bacf3c01bfafbc038496619b1bb4849433f7
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2022-07-08 15:33:46 -0500

    Create SessionListener.php

---
+++ src/simpleffa/listener/SessionListener.php
@@ -0,0 +1,39 @@
+<?php
+/* 
+ * Copyright (C) Sergittos - All Rights Reserved 
+ * Unauthorized copying of this file, via any medium is strictly prohibited 
+ * Proprietary and confidential 
+ */
+
+declare(strict_types=1);
+
+
+namespace simpleffa\listener;
+
+use pocketmine\player\Player;
+
+class SessionFactory {
+
+    /** @var Session[] */
+    static private array $sessions = [];
+
+    /**
+     * @return Session[]
+     */
+    static public function getSessions(): array {
+        return self::$sessions;
+    }
+
+    static public function getSession(Player $player): ?Session {
+        return self::$sessions[$player->getName()] ?? null;
+    }
+
+    static public function createSession(Player $player): void {
+        self::$sessions[$player->getName()] = new Session($player);
+    }
+
+    static public function removeSession(Player $player): void {
+        unset(self::$sessions[$player->getName()]);
+    }
+
+}
